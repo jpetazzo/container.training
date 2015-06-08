a6acbce1e7445e354e52cfea56a3bbeb9180a352
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2015-06-08 08:40:13 -0700

    Fix webui

--- dockercoins/webui/webui.js
+++ dockercoins/webui/webui.js
@@ -6,29 +6,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/json', function (req, res) {
-    redis.lrange('timing', 0, -1, function (err, timing) {
-        redis.hlen('wallet', function (err, coins) {
-            timing.reverse();
-            var speed10 = '?';
-            var speed100 = '?';
-            var speed1000 = '?';
-            if (timing.length >= 10) {
-                speed10 = 10 / (timing[0]-timing[9]);
-            }
-            if (timing.length >= 100) {
-                speed100 = 100 / (timing[0]-timing[99]);
-            }
-            if (timing.length >= 1000) {
-                speed1000 = 1000 / (timing[0]-timing[999]);
-            }
+    redis.hlen('wallet', function (err, coins) {
+        redis.get('hashes', function (err, hashes) {
             var now = Date.now() / 1000;
             res.json( {
-                speed10: speed10,
-                speed100: speed100,
-                speed1000: speed1000,
                 coins: coins,
-                now: now,
-                ago: now - timing[0]
+                hashes: hashes,
+                now: now
             });
         });
     });
