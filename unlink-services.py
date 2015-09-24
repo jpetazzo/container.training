d9829f55eafd642e6add36c2a5048dabf374424a
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2015-09-24 16:49:26 +0000

    Fix typo

--- unlink-services.py
+++ unlink-services.py
@@ -7,7 +7,7 @@ import yaml
 # - with 1 parameter, the same file will be used for in and out
 # - with 2 parameters, the 1st is the input, the 2nd the output
 input_file = sys.argv[1]
-output_file = sys.argv[:1]
+output_file = sys.argv[-1]
 
 stack = yaml.load(open(input_file))
 
