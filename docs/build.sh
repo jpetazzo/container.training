b3a7e36c374a8f10b100737bbaeca45844c9c234
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2017-10-13 19:23:00 +0200

    fixup build.sh script

--- docs/build.sh
+++ docs/build.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
-
-for YAML in *.yml; do
-  ./markmaker.py < $YAML > $YAML.html || rm $YAML.html
-done
-
-if [ "$1" = "watch" ]; then
-  while true; do
-    find . | entr -d $0 && break
+case "$1" in
+once)
+  for YAML in *.yml; do
+    ./markmaker.py < $YAML > $YAML.html || rm $YAML.html
   done
-fi
+  ;;
+
+forever)
+  # There is a weird bug in entr, at least on MacOS,
+  # where it doesn't restore the terminal to a clean
+  # state when exitting. So let's try to work around
+  # it with stty.
+  STTY=$(stty -g)
+  while true; do
+    find . | entr -d $0 once
+    [ $? = 2 ] || break
+  done
+  stty $STTY
+  ;;
+
+*)
+  echo "$0 <once|forever>"
+  ;;
+esac
