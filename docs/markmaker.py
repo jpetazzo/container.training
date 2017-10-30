03561f38d83655362a96a532e29e86baf7a354de
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2017-10-30 09:10:30 -0700

    I'd like to close my tab and I left 4 spaces

--- docs/markmaker.py
+++ docs/markmaker.py
@@ -143,7 +143,7 @@ try:
         repo = os.environ["REPOSITORY_URL"]
     else:
         repo = subprocess.check_output(["git", "config", "remote.origin.url"])
-        repo = repo.strip().replace("git@github.com:", "https://github.com/")
+    repo = repo.strip().replace("git@github.com:", "https://github.com/")
     if "BRANCH" in os.environ:
         branch = os.environ["BRANCH"]
     else:
