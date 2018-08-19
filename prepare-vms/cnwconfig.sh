984136244cc8cabfe1a191cc9889b2fcd8c00fa4
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2018-08-19 21:18:17 +0100

    Updated generation script

--- prepare-vms/cnwconfig.sh
+++ prepare-vms/cnwconfig.sh
@@ -1,7 +1,11 @@
-export WORKSHOPNAME=$1
-./workshopctl deploy $WORKSHOPNAME settings/workshop-settings.yaml
-./workshopctl pull_images $WORKSHOPNAME
-./workshopctl cards $WORKSHOPNAME settings/workshop-settings.yaml
-./workshopctl list
-./workshopctl list $WORKSHOPNAME
-# ./workshopctl stop $WORKSHOPNAME
+if [ $# -eq 0 ]
+then
+	echo "A shell script to configure nodes for a kubernetes workshop."
+	echo "Usage : $0 name of workshop"
+	exit 1
+fi
+
+./workshopctl deploy $1 settings/workshop-settings.yaml
+./workshopctl pull_images $1
+./workshopctl kube $1
+./workshopctl cards $1 settings/workshop-settings.yaml
--- prepare-vms/cnwsetup.sh
+++ prepare-vms/cnwsetup.sh
@@ -1,9 +1,14 @@
-export WORKSHOPNAME=$1
-cd prepare-vms
+if [ $# -eq 0 ]
+then
+	echo "A shell script to configure nodes for a kubernetes workshop."
+	echo "Usage : $0 name of workshop"
+	exit 1
+fi
+
 docker-compose build
-az group create --name $WORKSHOPNAME --location eastus
-az group deployment create --resource-group $WORKSHOPNAME --template-file azuredeploy.json --parameters @azuredeploy.parameters.json
-az vm list-ip-addresses --resource-group $WORKSHOPNAME --output table
-mkdir -p tags/$WORKSHOPNAME
-az vm list-ip-addresses --resource-group $WORKSHOPNAME --output json | jq -r '.[].virtualMachine.network.publicIpAddresses[].ipAddress' > tags/$WORKSHOPNAME/ips.txt
-# az group delete --resource-group $WORKSHOPNAME -y
+az group create --name $1 --location eastus
+az group deployment create --resource-group $1 --template-file azuredeploy.json --parameters @azuredeploy.parameters.json
+az vm list-ip-addresses --resource-group $1 --output table
+mkdir -p tags/$1
+az vm list-ip-addresses --resource-group $1 --output json | jq -r '.[].virtualMachine.network.publicIpAddresses[].ipAddress' > tags/$WORKSHOPNAME/ips.txt
+# az group delete --resource-group $1 -y
