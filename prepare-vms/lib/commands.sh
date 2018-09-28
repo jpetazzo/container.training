7f6a15fbb77e9ac4e879104d27abee920e446f1d
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2018-09-28 18:18:05 -0500

    Actually modify the prompt

--- prepare-vms/lib/commands.sh
+++ prepare-vms/lib/commands.sh
@@ -150,7 +150,7 @@ _cmd_kube() {
     sudo ln -sf /home/ubuntu/kubectx/kubens /usr/local/bin/kns &&
     sudo cp /home/ubuntu/kubectx/completion/*.bash /etc/bash_completion.d &&
     [ -d kube-ps1 ] || git clone https://github.com/jonmosco/kube-ps1 &&
-    sudo -u docker sed s/docker-prompt/kube_ps1/ /home/docker/.bashrc &&
+    sudo -u docker sed -i s/docker-prompt/kube_ps1/ /home/docker/.bashrc &&
     sudo -u docker tee -a /home/docker/.bashrc <<EOF
 . /home/ubuntu/kube-ps1/kube-ps1.sh
 KUBE_PS1_PREFIX=""
