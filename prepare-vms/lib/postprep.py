83d78677e50af7b915d5892d31bcff88c84805ba
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2019-06-02 23:21:44 -0500

    Update postprep.py

--- prepare-vms/lib/postprep.py
+++ prepare-vms/lib/postprep.py
@@ -61,9 +61,10 @@ system("id docker || sudo useradd -d /home/docker -m -s /bin/bash docker")
 system("echo docker:{} | sudo chpasswd".format(DOCKER_USER_PASSWORD))
 
 # Fancy prompt courtesy of @soulshake.
+# We set KUBE_PS1_ENABLED to avoid warnings when using "set -u".
 system("""sudo -u docker tee -a /home/docker/.bashrc <<SQRL
 export PS1='\e[1m\e[31m[{}] \e[32m(\\$(docker-prompt)) \e[34m\u@\h\e[35m \w\e[0m\n$ '
-export KUBE_PS1_ENABLED='off'
+export KUBE_PS1_ENABLED='on'
 SQRL""".format(ipv4))
 
 # Custom .vimrc
