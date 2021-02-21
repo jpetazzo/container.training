2516b2d32bd741f3ca1e953a05430670a4206c19
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2021-02-21 16:29:49 +0100

    🐞 Fix Helm command in Prom deploy

--- prepare-vms/lib/commands.sh
+++ prepare-vms/lib/commands.sh
@@ -721,7 +721,7 @@ _cmd_helmprom() {
     need_tag
     pssh "
     if i_am_first_node; then
-        sudo -u docker -H helm helm repo add prometheus-community https://prometheus-community.github.io/helm-charts/
+        sudo -u docker -H helm repo add prometheus-community https://prometheus-community.github.io/helm-charts/
         sudo -u docker -H helm install prometheus prometheus-community/prometheus \
             --namespace kube-system \
             --set server.service.type=NodePort \
