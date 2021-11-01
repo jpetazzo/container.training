7fc2d5e57f1d12d9b5782b67e5e52c2545d2a8fb
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2021-11-01 06:55:21 +0100

    🖨️  Improve Terraform outputs and install metrics-server
    Stage2 output should now be easier to copy-paste to a Google Spreadsheet.
    Add Helm support and use it to deploy metrics-server on each cluster.

--- prepare-tf/main.tf
+++ prepare-tf/main.tf
@@ -1,5 +1,5 @@
 module "clusters" {
-  source             = "./modules/scaleway"
+  source             = "./modules/linode"
   for_each           = local.clusters
   cluster_name       = each.value.cluster_name
   min_nodes_per_pool = var.min_nodes_per_pool
@@ -13,16 +13,17 @@ locals {
   clusters = {
     for i in range(101, 101 + var.how_many_clusters) :
     i => {
-      cluster_name        = format("%s-%03d", local.tag, i)
-      kubeconfig_path     = format("./stage2/kubeconfig.%03d", i)
-      dashdash_kubeconfig = format("--kubeconfig=./stage2/kubeconfig.%03d", i)
-      externalips_path    = format("./stage2/externalips.%03d", i)
+      cluster_name    = format("%s-%03d", local.tag, i)
+      kubeconfig_path = format("./stage2/kubeconfig.%03d", i)
+      #dashdash_kubeconfig = format("--kubeconfig=./stage2/kubeconfig.%03d", i)
+      externalips_path = format("./stage2/externalips.%03d", i)
     }
   }
 }
 
 resource "local_file" "stage2" {
   filename = "./stage2/main.tf"
+  file_permission = "0644"
   content = templatefile(
     "./stage2.tmpl",
     { clusters = local.clusters }
@@ -30,16 +31,28 @@ resource "local_file" "stage2" {
 }
 
 resource "local_file" "kubeconfig" {
-  for_each = local.clusters
-  filename = each.value.kubeconfig_path
-  content  = module.clusters[each.key].kubeconfig
+  for_each        = local.clusters
+  filename        = each.value.kubeconfig_path
+  file_permission = "0600"
+  content         = module.clusters[each.key].kubeconfig
+}
+
+resource "local_file" "externalips" {
+  for_each        = local.clusters
+  filename        = each.value.externalips_path
+  file_permission = "0600"
+  content         = ""
 
   provisioner "local-exec" {
+    environment = {
+      KUBECONFIG = local_file.kubeconfig[each.key].filename
+    }
+
     command = <<-EOT
-      kubectl ${each.value.dashdash_kubeconfig} get nodes --watch \
+      kubectl get nodes --watch \
       | grep --silent --line-buffered . \
-      && kubectl ${each.value.dashdash_kubeconfig} wait node --for=condition=Ready --all --timeout=10m \
-      && kubectl ${each.value.dashdash_kubeconfig} get nodes \
+      && kubectl wait node --for=condition=Ready --all --timeout=10m \
+      && kubectl get nodes \
       -o 'jsonpath={.items[*].status.addresses[?(@.type=="ExternalIP")].address}' > ${each.value.externalips_path}
       EOT
   }
