311a2aaf32fea1324ba0c8a8e1e4bf619c5e568f
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2023-10-10 07:37:56 +0200

    🔧 Add scaleway invocation to konk script

--- prepare-labs/konk.sh
+++ prepare-labs/konk.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 # deploy big cluster
-TF_VAR_node_size=g6-standard-6 \
+#TF_VAR_node_size=g6-standard-6 \
+#TF_VAR_nodes_per_cluster=5 \
+#TF_VAR_location=eu-west \
+
+TF_VAR_node_size=PRO2-XS \
 TF_VAR_nodes_per_cluster=5 \
-TF_VAR_location=eu-west \
-./labctl create --mode mk8s --settings settings/mk8s.env --provider linode --tag konk
+TF_VAR_location=fr-par-2 \
+./labctl create --mode mk8s --settings settings/mk8s.env --provider scaleway --tag konk
 
 # set kubeconfig file
 cp tags/konk/stage2/kubeconfig.101 ~/kubeconfig
@@ -16,4 +20,4 @@ while read node address; do
 done
 
 # vcluster all the things
-./labctl create --settings settings/mk8s.env --provider vcluster --mode mk8s --students 27
+./labctl create --settings settings/mk8s.env --provider vcluster --mode mk8s --students 50
