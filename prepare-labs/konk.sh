06ca097b5293be9e651f9e5c24642b6882505174
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2024-05-10 15:10:18 +0200

    🔧 Update konk deployment script

--- prepare-labs/konk.sh
+++ prepare-labs/konk.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
+PROVIDER=scaleway
 
-# deploy big cluster
-#TF_VAR_node_size=g6-standard-6 \
-#TF_VAR_nodes_per_cluster=5 \
-#TF_VAR_location=eu-west \
+case "$PROVIDER" in
+linode)
+  export TF_VAR_node_size=g6-standard-6
+  export TF_VAR_location=eu-west
+  ;;
+scaleway)
+  export TF_VAR_node_size=PRO2-XS
+  export TF_VAR_location=fr-par-2
+  ;;
+esac
 
-TF_VAR_node_size=PRO2-XS \
-TF_VAR_nodes_per_cluster=5 \
-TF_VAR_location=fr-par-2 \
-./labctl create --mode mk8s --settings settings/mk8s.env --provider scaleway --tag konk
+./labctl create --mode mk8s --settings settings/konk.env --provider $PROVIDER --tag konk
 
 # set kubeconfig file
-cp tags/konk/stage2/kubeconfig.101 ~/kubeconfig
-
 export KUBECONFIG=~/kubeconfig
+cp tags/konk/stage2/kubeconfig.101 $KUBECONFIG
 
 # set external_ip labels
 kubectl get nodes -o=jsonpath='{range .items[*]}{.metadata.name} {.status.addresses[?(@.type=="ExternalIP")].address}{"\n"}{end}' |
@@ -22,4 +25,4 @@ while read node address; do
 done
 
 # vcluster all the things
-./labctl create --settings settings/mk8s.env --provider vcluster --mode mk8s --students 50
+./labctl create --settings settings/mk8s.env --provider vcluster --mode mk8s --students 30
