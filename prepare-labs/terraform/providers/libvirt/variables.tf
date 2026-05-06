26d2b17e5ce482c9db9a82c415cf80bfbb211828
fccagou <me@fccagou.fr>
2026-05-07 00:49:46 +0200

    prepare-labs: add terraform libvirt provider
    It's the first version working for only 1 student/cluster.
    The code contains `TODO:` labels to remind some ideas for future
    versions _( grep -r "TODO:" prepare-labs/terraform/*/libvirt)_
    
    Running testing command on laptop:
    
         TF_VAR_nodes_per_cluster=2 \
         TF_VAR_node_size=S \
         ./labctl create \
            --students 1 \
            --settings settings/kubernetes.env \
            --provider libvirt  --tag container-training
    
    To destroy , run
    
         ( TAG=container-training ; ./labctl destroy "$TAG" ; rm -f tags/"$TAG"/* )
    
    Signed-off-by: fccagou <me@fccagou.fr>

---
+++ prepare-labs/terraform/providers/libvirt/variables.tf
@@ -0,0 +1,18 @@
+# vCPUs and RAM (in MiB) are encoded as "vcpus ram" to keep the same
+# pattern used by other providers (e.g. proxmox) in this project.
+variable "node_sizes" {
+  type = map(any)
+  default = {
+    XS = "1 512"
+    S = "1 2048"
+    M = "2 4096"
+    L = "4 8192"
+  }
+}
+
+variable "location" {
+  type    = string
+  default = ""
+  # TODO: make the location depending on libvirt_uri
+  description = "Unused for libvirt (local hypervisor). Kept for interface consistency."
+}
